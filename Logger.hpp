14c7b1da3c03336c02fa3925c57f49eeca8b6517
radonyi <robert.adonyi@gmail.com>
2023-06-29 22:54:53 +0200

    Add console logger

---
+++ Logger.hpp
@@ -0,0 +1,42 @@
+#ifndef LOGGER_H_INCLUDED
+#define LOGGER_H_INCLUDED
+
+#include <iostream>
+#include <sstream>
+#include <string>
+
+inline std::string className(const std::string& prettyFunction)
+{
+  size_t colons = prettyFunction.rfind("::");
+  if (colons == std::string::npos) { 
+    return "";
+  }
+  size_t begin = prettyFunction.substr(0,colons).rfind(" ") + 1;
+  size_t end = colons - begin;
+
+  return prettyFunction.substr(begin,end);
+}
+
+struct scoped_logger
+{
+  scoped_logger(const std::string& className, const char* funcName, unsigned line)
+    : _ss()
+  {
+    char name[8];
+    pthread_getname_np(pthread_self(), &name[0], sizeof(name));
+    _ss << "[" << name << "," << getpid() << "] " << className << "::" << funcName << "(" << line << "): ";
+  }
+
+  std::stringstream& stream(){ return _ss; }
+  ~scoped_logger(){ std::cout << "\n" << _ss.str(); }
+private:
+  std::stringstream _ss;
+};
+
+#define __CLASS_NAME__ className(__PRETTY_FUNCTION__)
+
+#define LOG(What) \
+  scoped_logger(__CLASS_NAME__, __FUNCTION__, __LINE__).stream() << What
+
+
+#endif // LOGGER_H_INCLUDED
--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 
 #include <iostream>
+#include "Logger.hpp"
 
 int main() {
-  std::cout << "Hello world";
+  pthread_setname_np(pthread_self(), "TH_MAIN");
+
+  LOG("Start");
+  LOG("End");
   return 0;
 }
